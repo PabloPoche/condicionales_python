183392430ef86e6ca32f4c9feb63e2404a05f5a3
PabloPoche <107163780+PabloPoche@users.noreply.github.com>
2022-06-23 08:04:42 -0300

    Update profundizacion_1.py

--- ejercicios_profunidzacion/profundizacion_1.py
+++ ejercicios_profunidzacion/profundizacion_1.py
@@ -19,6 +19,15 @@ Realice un programa que solicite por consola 2 números
 Calcule la diferencia entre ellos e informe por pantalla
 si el resultado es positivo, negativo o cero.
 '''
-
 print('Ejercicios de práctica con números')
 # Empezar aquí la resolución del ejercicio
+numero_1 = int(input("Ingrese el primer numero:"))
+numero_2 = int(input("Ingrese el segundo numero:"))
+resta = numero_1 - numero_2
+if resta == 0 :
+  print("El resultado de la resta es 0")
+elif resta > 0 :
+  print("El resultado de la resta es positivo")
+  else:
+  print("El resultado de la resta es negativo")
+  
