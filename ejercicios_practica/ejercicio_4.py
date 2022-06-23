6252338ff857ce03f18c599147cae2e48c321381
PabloPoche <107163780+PabloPoche@users.noreply.github.com>
2022-06-23 07:52:50 -0300

    Update ejercicio_4.py

--- ejercicios_practica/ejercicio_4.py
+++ ejercicios_practica/ejercicio_4.py
@@ -16,19 +16,32 @@ texto_2 = '7'
 # La comparación alfabética es aquella que se logra cuando
 # se utiliza el operador mayor o menor con Strings (textos)
 # Imprima en pantalla según corresponda
-
+if texto_1 > texto_2 :
+  print("texto 1 > texto 2")
+else:
+   print("texto 2 > texto 1")  
+    
 # 2-Transforma esas variables tipo texto en variables numéricas con (int)
 # y almacénalas en nuevas variables.
 # Compare las nuevas variables para ver cual es mayor o menor
 # utilizando los operadores correspondientes
 # ¿Cuál de las nuevas variables es mayor?
 # Imprima en pantalla según corresponda
+int_texto_1 = int(texto_1)
+int_texto_2 = int(texto_2)
 
+if int_texto_1 > int_texto_2 :
+  print("Valor numerico del texto 1 > texto 2")
+else:
+   print("Valor numerico del texto 2 > texto 1")
 # Para pensar!
 # ¿Por qué cree que texto_2 es mayor a texto_1?
 # Siendo números tiene sentido, pero son caracteres, texto,
 # aún así el operador arroja el mismo resultado que con las
 # variables numéricas, cierto? ¿Por qué creen que es así?
+
+# EL VALOR NUMERICO DE UN STRING ESTA DADO SEGUN SU POSICION EN LA TABLA ASCII("5"=29 Y "7"=31)
+
 # Esta pregunta estará repetida en el Campus para que puedan
 # responder.
 # NOTA: La respuesta no se encuentra en el apunte, sino en Google ;)
